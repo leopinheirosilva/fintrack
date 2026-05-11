dfbf0995cca583f78ccbae3cc4db1f87db376ef6
Leonardo <leonardopinheirosilva16@gmail.com>
2026-05-11 07:40:41 -0300

    feat: add checkbox terms to signup form

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -10,6 +10,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
 
 const SignupPage = () => {
@@ -27,11 +28,27 @@ const SignupPage = () => {
           <Input placeholder="Digite o seu e-mail" />
           <PasswordInput />
           <PasswordInput placeholder="Digite sua senha novamente" />
+          {/* Termos e Condições */}
+          <div className="items-top flex space-x-2 pt-3">
+            <Checkbox id="terms" />
+            <div className="grid gap-1.5 leading-none">
+              <label
+                htmlFor="terms"
+                className="text-xs font-medium leading-none text-muted-foreground opacity-75 peer-disabled:cursor-not-allowed"
+              >
+                Ao clicar em &quot;Criar conta&quot;, você aceita{' '}
+                <a href="#" className="text-white underline">
+                  nosso termo de uso e política de privacidade
+                </a>
+              </label>
+            </div>
+          </div>
         </CardContent>
         <CardFooter>
           <Button className="w-full">Criar conta</Button>
         </CardFooter>
       </Card>
+      {/* Login */}
       <div className="flex items-center justify-center">
         <p className="text-center opacity-50">Ja possui uma conta?</p>
         <Button variant="link" asChild className="p-2">
