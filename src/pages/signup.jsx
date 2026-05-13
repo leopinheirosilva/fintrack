f7cfe644c61f72a7e145ad7b4b0dc74716b8e306
Leonardo <leonardopinheirosilva16@gmail.com>
2026-05-13 15:38:29 -0300

    feat: ensure password and password confirmation are the same in signup form

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -28,26 +28,32 @@ import { Input } from '@/components/ui/input'
 import { api } from '@/lib/axios'
 
 // regras para validação de campos do formulário com zod
-const signupSchema = z.object({
-  firstName: z.string().trim().min(1, {
-    message: 'O nome é obrigatório',
-  }),
-  lastName: z.string().trim().min(1, {
-    message: 'O sobrenome é obrigatório',
-  }),
-  email: z.string().email({
-    message: 'O e-mail é inválido',
-  }),
-  password: z.string().trim().min(6, {
-    message: 'A senha deve ter no mínimo 6 caracteres',
-  }),
-  passwordConfirmation: z.string().trim().min(6, {
-    message: 'A confirmação de senha é obrigatória',
-  }),
-  terms: z.boolean().refine((value) => value == true, {
-    message: 'Campo obrigatório',
-  }),
-})
+const signupSchema = z
+  .object({
+    firstName: z.string().trim().min(1, {
+      message: 'O nome é obrigatório',
+    }),
+    lastName: z.string().trim().min(1, {
+      message: 'O sobrenome é obrigatório',
+    }),
+    email: z.string().email({
+      message: 'O e-mail é inválido',
+    }),
+    password: z.string().trim().min(6, {
+      message: 'A senha deve ter no mínimo 6 caracteres',
+    }),
+    passwordConfirmation: z.string().trim().min(6, {
+      message: 'A confirmação de senha é obrigatória',
+    }),
+    terms: z.boolean().refine((value) => value == true, {
+      message: 'Campo obrigatório',
+    }),
+  })
+  // lógica para confirmação de senha
+  .refine((data) => data.password == data.passwordConfirmation, {
+    message: 'As senhas não coincidem',
+    path: ['passwordConfirmation'],
+  })
 
 const SignupPage = () => {
   const [user, setUser] = useState(null)
