814fce7c91b0cb7a194aadd90cea910765d4e7d5
Leonardo <leonardopinheirosilva16@gmail.com>
2026-05-17 21:31:52 -0300

    feat: ensure calengar language is pt-BR

--- src/components/ui/date-picker-with-range.jsx
+++ src/components/ui/date-picker-with-range.jsx
@@ -60,6 +60,7 @@ export const DatePickerWithRange = ({
             selected={value}
             onSelect={onChange}
             numberOfMonths={2}
+            locale={ptBR}
           />
         </PopoverContent>
       </Popover>
