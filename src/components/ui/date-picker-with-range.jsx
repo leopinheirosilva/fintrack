d8ad127b17b9da6e0551dcff458d8ed5aabc0b2e
Leonardo <leonardopinheirosilva16@gmail.com>
2026-05-17 21:29:31 -0300

    feat: ensure date range can be selected on home page

--- src/components/ui/date-picker-with-range.jsx
+++ src/components/ui/date-picker-with-range.jsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { format } from 'date-fns'
-import { Calendar as CalendarIcon } from 'lucide-react'
+import { ptBR } from 'date-fns/locale/pt-BR'
+import { CalendarIcon } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 import { Calendar } from '@/components/ui/calendar'
@@ -12,33 +13,56 @@ import {
 } from '@/components/ui/popover'
 import { cn } from '@/lib/utils'
 
-export const DatePickerDemo = ({
-  onChange,
+export const DatePickerWithRange = ({
   value,
-  placeholder = 'selectione uma data',
+  onChange,
+  className,
+  placeholder = 'Selecione um mês',
 }) => {
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          className={cn(
-            'w-[280px] justify-start text-left font-normal',
-            !value && 'text-muted-foreground'
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {value ? format(value, 'PPP') : <span>{placeholder}</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
-        <Calendar
-          mode="single"
-          selected={value}
-          onSelect={onChange}
-          initialFocus
-        />
-      </PopoverContent>
-    </Popover>
+    <div className={cn('grid gap-2', className)}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            id="date-picker-range"
+            className={cn(
+              'w-full justify-start text-left font-normal',
+              !value && 'text-muted-foregournd'
+            )}
+          >
+            <CalendarIcon />
+            {value?.from ? (
+              value.to ? (
+                <>
+                  {format(value.from, 'LLL dd, y', {
+                    locale: ptBR,
+                  })}
+                  -{' '}
+                  {format(value.to, 'LLL dd, y', {
+                    locale: ptBR,
+                  })}
+                </>
+              ) : (
+                format(value.from, 'LLL dd, y', {
+                  locale: ptBR,
+                })
+              )
+            ) : (
+              <span>{placeholder}</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            mode="range"
+            defaultMonth={value?.from}
+            selected={value}
+            onSelect={onChange}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,9 @@
+import { PlusIcon } from 'lucide-react'
 import { Navigate } from 'react-router'
 
+import DateSelection from '@/components/date-selection'
 import Header from '@/components/header'
+import { Button } from '@/components/ui/button'
 import { useAuthContext } from '@/contexts/auth'
 
 const HomePage = () => {
@@ -15,6 +18,19 @@ const HomePage = () => {
   return (
     <div>
       <Header />
+      <div className="p-8">
+        {/* PARTE DO TOPO */}
+        <div className="flex items-center justify-between">
+          <h2 className="text-2xl font-bold">Dashboard</h2>
+          <div className="flex items-center gap-2">
+            {/* SELETOR DE DATA E BOTAO DE NOVA TRANSAÇÃO */}
+            <DateSelection />
+            <Button>
+              <PlusIcon /> Nova transação
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
