ab1b667d5cb3177de1d4dd110b7e27d14937322c
Leonardo <leonardopinheirosilva16@gmail.com>
2026-05-20 07:09:41 -0300

    feat: ensure from and to query params are only considered if they were valid

--- src/components/date-selection.jsx
+++ src/components/date-selection.jsx
@@ -1,5 +1,5 @@
 import { useQueryClient } from '@tanstack/react-query'
-import { addMonths, format } from 'date-fns'
+import { addMonths, format, isValid } from 'date-fns'
 import { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router'
 
@@ -9,19 +9,34 @@ import { DatePickerWithRange } from './ui/date-picker-with-range'
 
 const formatDateToQueryParam = (date) => format(date, 'yyyy-MM-dd')
 
+const getInitialDateState = (searchParams) => {
+  const from = searchParams.get('from')
+  const to = searchParams.get('to')
+  const defaultDate = {
+    from: new Date(),
+    to: addMonths(new Date(), 1),
+  }
+  // se o "from" e o "to" não existirem, retorna default
+  if (!from || !to) {
+    return defaultDate
+  }
+  // se o "from" e o "to" não forem válidos, retorna default
+  if (!isValid(new Date(from)) || !isValid(new Date(to))) {
+    return defaultDate
+  }
+  // se o "from" e o "to" forem válidos, retorna a data selecionada
+  return {
+    from: new Date(searchParams.get('from') + 'T00:00:00'),
+    to: new Date(searchParams.get('to') + 'T00:00:00'),
+  }
+}
+
 const DateSelection = () => {
   const [searchParams] = useSearchParams()
   const navigate = useNavigate()
   const queryClient = useQueryClient()
   const { user } = useAuthContext()
-  const [date, setDate] = useState({
-    from: searchParams.get('from')
-      ? new Date(searchParams.get('from') + 'T00:00:00')
-      : new Date(),
-    to: searchParams.get('to')
-      ? new Date(searchParams.get('to') + 'T00:00:00')
-      : addMonths(new Date(), 1),
-  })
+  const [date, setDate] = useState(getInitialDateState(searchParams))
 
   // persiste o state "date" na URL (?from&to=) e atualiza o state com esse valor quando a página é carregada
   useEffect(() => {
