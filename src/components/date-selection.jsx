7b4f0292193726aa204519e0084228dbf41448ea
Leonardo <leonardopinheirosilva16@gmail.com>
2026-05-20 06:55:26 -0300

    feat: ensure from and to params are passed to balance query key

--- src/components/date-selection.jsx
+++ src/components/date-selection.jsx
@@ -33,7 +33,12 @@ const DateSelection = () => {
     queryParams.set('to', formatDateToQueryParam(date.to))
     navigate(`/?${queryParams.toString()}`)
     queryClient.invalidateQueries({
-      queryKey: ['balance', user.id],
+      queryKey: [
+        'balance',
+        user.id,
+        formatDateToQueryParam(date.from),
+        formatDateToQueryParam(date.to),
+      ],
     })
   }, [navigate, date, queryClient, user.id])
 
