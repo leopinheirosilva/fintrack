a8177c0abe3ce527ef301e37bedffb77f59451f5
Leonardo <leonardopinheirosilva16@gmail.com>
2026-05-21 07:02:24 -0300

    docs: change JS Docs on transaction service

--- src/api/services/transaction.js
+++ src/api/services/transaction.js
@@ -3,11 +3,11 @@ import { protectedApi } from '@/lib/axios'
 export const TransactionService = {
   /**
    * Cria uma transação para o usuário autenticado
-   * @param {*} input
+   * @param {*} input - Transação a ser criada
    * @param {*} input.name - Nome da transação
    * @param {*} input.date - Data da transação (YYYY-MM-DD)
    * @param {*} input.amount - Valor da transação
-   * @param {*} input.type - Tipo da transação (EARNING/EXPENSE/INVESTMENT)
+   * @param {*} input.type - Tipo da transação (EARNING, EXPENSE, INVESTMENT)
    */
   create: async (input) => {
     const response = await protectedApi.post('/transactions/me', input)
